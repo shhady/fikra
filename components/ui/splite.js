fa3bc61f0954e27b27cf76ad643ffa8fcee83c06
shhady <shhadyse@gmail.com>
2025-11-17 16:33:16 +0200

    robot

--- components/ui/splite.js
+++ components/ui/splite.js
@@ -1,20 +1,28 @@
 'use client'
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Script from 'next/script'
 
 export function SplineScene({ scene, className }) {
+  const [mounted, setMounted] = useState(false)
+  useEffect(() => setMounted(true), [])
+
   return (
     <>
       <Script
-        src="https://unpkg.com/@splinetool/runtime/build/spline-viewer.js"
+        src="https://unpkg.com/@splinetool/viewer@1.9.37/build/spline-viewer.js"
+        type="module"
         strategy="afterInteractive"
       />
-      <spline-viewer
-        url={scene}
-        class={className}
-        style={{ width: '100%', height: '100%', display: 'block' }}
-      />
+      {mounted ? (
+        <spline-viewer
+          url={scene}
+          className={className}
+          style={{ width: '100%', height: '100%', display: 'block' }}
+        />
+      ) : (
+        <div className="w-full h-full" />
+      )}
     </>
   )
 }
